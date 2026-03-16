7783911391064e56818fb65454321b425388b5d6
unknown <keerthan-gowda-c>
2026-03-16 21:32:16 +0530

    buttons added

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -4,35 +4,35 @@ const ProductCard = ({ product }) => {
 
     return (
         <>
-           
-                <div >
-                   
-                        <div className="card">
-                            <img src={product.thumbnail} />
-                            <h4 className='text-center'>
-                                {product.product_title}
-                            </h4>
-                            <p className='text-center'>
-                                {product.product_description}
-                            </p>
-                            <p >
-                                {product.product_price}
-                            </p>
-                            <div className='buttons'>
- <button className='btn btn-warning'>
-                                Add to Bag
-                            </button>
-                            <button className='btn btn-success'>
-                                Order Now
-                            </button>
-                            </div>
-                           
 
-                        </div>
-                   
+            <div >
+
+                <div className="card">
+                    <img src={product.thumbnail} />
+                    <h4 className='text-center'>
+                        {product.product_title}
+                    </h4>
+                    <p className='text-center'>
+                        {product.product_description}
+                    </p>
+                    <p >
+                        {product.product_price}
+                    </p>
+                    <div className='buttons'>
+                        <button className='btn btn-warning'>
+                            Add to Bag
+                        </button>
+                        <button className='btn btn-success'>
+                            Order Now
+                        </button>
+                    </div>
+
+
                 </div>
 
-            
+            </div>
+
+
         </>
     )
 }
--- frontend/src/data/carousel_data/carousel_data.jsx
+++ frontend/src/data/carousel_data/carousel_data.jsx
@@ -1,14 +1,14 @@
 const carousel_data=[
 {
     id:1,
-    img_src:'images/product/paneerBiryani2.jpeg',
+    img_src:'images/product/biryani1.avif',
     img_title:'Special Paneer Biryani',
     img_description:'Special Paneer Biryani — where every grain is rich, aromatic, and irresistibly flavorful.',
     alt:'Special Paneer Biryani',
 },
 {
     id:2,
-    img_src:'images/product/paneerBiryani1.jpeg',
+    img_src:'images/product/biryani2.avif',
     img_title:'Paneer Biryani',
     img_description:'Fragrant layers of spice and soft paneer—biryani bliss in every bite.',
     alt:'Paneer Biryani',
@@ -23,19 +23,50 @@ const carousel_data=[
 },
 {
     id:4,
-    img_src:'/images/product/biryani1.avif',
+    img_src:'/images/product/biryani4.jpg',
     img_title:'Chicken Biryani',
     img_description:'A plate of chicken biryani that speaks the language of comfort and spice.',
     alt:'Chicken Biryani',
 },
 {
     id:5,
-    img_src:'images/product/biryani2.avif',
+    img_src:'images/product/biryani5.jpg',
+    img_title:'Dum Biryani',
+    img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
+    alt:'Dum Biryani',
+},
+{
+    id:6,
+    img_src:'images/product/biryani6.jpg',
+    img_title:'Dum Biryani',
+    img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
+    alt:'Dum Biryani',
+},
+{
+    id:7,
+    img_src:'images/product/biryani7.jpg',
+    img_title:'Dum Biryani',
+    img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
+    alt:'Dum Biryani',
+},{
+    id:8,
+    img_src:'images/product/biryani8.jpg',
+    img_title:'Dum Biryani',
+    img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
+    alt:'Dum Biryani',
+},{
+    id:9,
+    img_src:'images/product/paneerBiryani1.jpeg',
+    img_title:'Dum Biryani',
+    img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
+    alt:'Dum Biryani',
+},{
+    id:10,
+    img_src:'images/product/paneerBiryani2.jpeg',
     img_title:'Dum Biryani',
     img_description:'Sealed with spice, slow-cooked in love — dum biryani perfection.',
     alt:'Dum Biryani',
 }
-
 ]
 
 export default carousel_data;
